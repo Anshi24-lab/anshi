5a6a0723401d12a66d4a87ca637a05e1978972ed
Anshi24-lab <83134380+Anshi24-lab@users.noreply.github.com>
2023-01-13 03:36:01 +0530

    upload

---
+++ wiz3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+    int a, b;
+    a = 10;
+    b = 20;
+    int c =
+    if(a>b){
+        printf("A is greater");
+    }
+    else if(a==b){
+            printf("True");
+    }
+
+
+
+
+    return 0;
+}
+
